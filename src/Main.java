ffd04ddb43a8e14dd1017ab35f5f9c94569157ea
Tatsuo Matsuyoshi <tatuo.matuyosi@gmail.com>
2025-05-10 12:45:21 +0900

    Add JAR build and upload to GitHub Actions

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 
 public class Main {
-    public static int add(int a, int b) {
-        return a + b;
-    }
-
     public static void main(String[] args) {
-         System.out.println("Hello, Java!");
+        System.out.println("Hello from JAR!");
     }
 }
