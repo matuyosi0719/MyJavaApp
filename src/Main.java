8766c2a6e50a64e3956c41edd099f78cbf17a142
Tatsuo Matsuyoshi <tatuo.matuyosi@gmail.com>
2025-05-10 12:36:44 +0900

    Ensure Main.java with add() method is committed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+
 public class Main {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, Java World!");
+         System.out.println("Hello, Java!");
     }
 }
