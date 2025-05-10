1181e8ffb3ebd0b32b1ea9d2aee520099d196d34
Tatsuo Matsuyoshi <tatuo.matuyosi@gmail.com>
2025-05-10 12:26:25 +0900

    Add JUnit and Hamcrest JARs

---
+++ test/MainTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void testAdd() {
+        assertEquals(5, Main.add(2, 3));
+    }
+}
+
